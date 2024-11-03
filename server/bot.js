0adb5c547fc78990a8cb8a9b17392b0af4e7a045
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-11-03 10:55:45 +0100

    test 2

--- server/bot.js
+++ server/bot.js
@@ -96,16 +96,18 @@ const scrapeLogic = async (res) => {
         await page.waitForSelector("p[ng-bind-html='$ctrl.pointProgressText']", { visible: true });
         const pointsbreakdown = await page.$eval("p[ng-bind-html='$ctrl.pointProgressText']", (x) => x.innerHTML);
         const maxSearches = pointsbreakdown?.includes("/ 30") ? 10 : 30; // 3 points per search
-
+        console.log("test1");
         const search_href = await page.$eval("#userPointsBreakdown a[mee-hyperlink]", (x) => x.getAttribute("href"));
         await page.goto(search_href, { waitUntil: "networkidle0" });
 
         const words = await import("random-words").then((randomWords) => randomWords.generate(maxSearches));
+        console.log("test2");
 
         // search first word
         await page.waitForSelector("input#sb_form_q", { visible: true, timeout: 10000 });
         await page.type("input#sb_form_q", words[0]);
         await page.keyboard.press("Enter");
+        console.log("test3");
 
         // for (let word of words.slice(1)) {
         //     await new Promise((resolve) => setTimeout(resolve, 3500));
@@ -116,10 +118,11 @@ const scrapeLogic = async (res) => {
         console.error("Error while running bot:", e);
     } finally {
         await browser.close();
+        console.log("closed");
     }
 
-    console.log("closed");
-    return;
+    console.log("test4");
+    process.exit(0);
 };
 
 scrapeLogic();
