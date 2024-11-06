664dff2db400f8ee188d5f7124530ede80a87ab0
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-11-06 15:46:57 +0100

    v3 test 3

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // Serve static files for the home page
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "login.html"));
