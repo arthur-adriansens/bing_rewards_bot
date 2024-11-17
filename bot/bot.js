a2c7f02993fe947936e5ca290552a05627662caa
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-11-17 10:16:09 +0000

    db bug test 3

--- bot/bot.js
+++ bot/bot.js
@@ -102,7 +102,9 @@ async function scrapeLogic(email, blobs) {
         await page.goto("https://rewards.bing.com", { waitUntil: "networkidle0" });
         await uploadScreenshot(page, email);
 
-        const points = await page.$$eval("#rewardsBanner mee-rewards-counter-animation span", (points) => points.map((x) => x.innerHTML));
+        const points = await page.$$eval("#rewardsBanner mee-rewards-counter-animation span", (points) =>
+            points.map((x) => x.innerHTML.replace(",", ""))
+        );
         console.log(points);
         console.log(`query: UPDATE botaccounts SET last_collected=NOW(), points=${points[0]}, streak=${points[2]} WHERE email=${email}`);
         await sql`UPDATE botaccounts SET last_collected=NOW(), points=${points[0]}, streak=${points[2]} WHERE email=${email}`;
